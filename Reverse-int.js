1ff23e10111f3def0c551565ebc7be933ab6d622
Mobasshir <mobasshirhussain2002@gmail.com>
2026-01-16 16:10:08 +0530

    rev

--- Reverse-int.js
+++ Reverse-int.js
@@ -5,8 +5,8 @@ let reverse =0;
 let numCopy=n;
 n=Math.abs(n)
 while(n>0){
-    reminder= n%10;
-    reverse=(10*reverse)+reminder;
+    remainder= n%10;
+    reverse=(10*reverse)+remainder;
     n=Math.floor(n/10)
 }
 return (numCopy<0) ? -reverse :reverse;
